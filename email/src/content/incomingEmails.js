7f664ff55d8e8cb09788f945aa0e6603d1373d32
Norma Driske <norma@entwicklerheld.de>
2020-12-23 22:28:45 +0100

    Update tapir emails

--- email/src/content/incomingEmails.js
+++ email/src/content/incomingEmails.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
+import logo from "@/assets/tpapir-logo.png";
 
 export default [
   {
@@ -7,14 +8,40 @@ export default [
     created: "",
     title: "👩🏼‍🔧 Successful registration at TPAPIR",
     content: `
-    Dear customer,<br/><br/>
-    thank you for your registration at TPAPIR.
-    <br/><br>
-    Your username is: <i>ineedtoiletpaper0349</i>
-    <br/><br>
-    Please take good care of your credentials.
-    <br/><br>
-    Your TPAPIR team`,
+    <div style="width: 500px; margin: 0 auto;">
+        <header style="border-bottom: 1px solid #efefef; padding: 20px 0 20px 0;">
+          <img src="${logo}" height="30"/><span style="margin-left: 10px; font-weight: 500; font-size: 20px; line-height: 20px; vertical-align:middle;">TPAPIR</span>
+          <div style="margin-top: 20px;"><i>It is not just paper, it is the expression of your innermost need.</i></div>
+        </header>
+        <div style="margin-top: 20px;">
+          Dear customer,<br/><br/>
+          thank you for your registration at TPAPIR.
+          <br/><br>
+          Your username is: <i>ineedtoiletpaper0349</i>
+          <br/><br>
+          Please take good care of your credentials.
+          <br/><br/>
+          And do not forget: With TPAPIR you back the right roll!<br/>
+          Have a fantastic day!<br/><br/>
+          Dylan for TPAPIR<br/>
+          <span style="font-size: 12px;"><i>Customer Markets Agent</i></span>
+          <br/><br/>
+          ---<br/>
+          TAPIR Inc.<br/>
+          Phone: +1 234 567 89 10<br/>
+          Fax: +1 234 567 89 10<br/>
+          Email: dylan@tpapir.rc3<br/>
+          
+          <div style="font-size: 12px; margin-top: 20px;">
+            <i>"A wonderful softness has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. And I wish the same for you!"</i><br>
+            - Jim Stone, Founder of TPAPIR
+          </div>
+        </div>
+        <div style="border-top: 1px solid #efefef; margin-top:20px; font-size:10px; color:#aaa; padding-top:10px">
+        This message is confidential and intended solely for the addressee. Any use by third parties is prohibited. If you have received the files in error, please contact the sender and delete the files from any computer and disk.
+        </div>
+      </div>
+    `,
     folder: "inbox",
     incoming: true
   },
@@ -24,10 +51,35 @@ export default [
     created: "",
     title: "👍 We received your order",
     content: `
-    Dear customer,<br/><br/>
-    we are happy to announce that we received your order.
-    <br/><br/>
-    Your TPAPIR team
+    <div style="width: 500px; margin: 0 auto;">
+        <header style="border-bottom: 1px solid #efefef; padding: 20px 0 20px 0;">
+          <img src="${logo}" height="30"/><span style="margin-left: 10px; font-weight: 500; font-size: 20px; line-height: 20px; vertical-align:middle;">TPAPIR</span>
+          <div style="margin-top: 20px;"><i>It is not just paper, it is the expression of your innermost need.</i></div>
+        </header>
+        <div style="margin-top: 20px;">
+          Dear customer,<br/><br/>
+          we are happy to announce that we received your order.
+          <br/><br/>
+          And do not forget: With TPAPIR you back the right roll!<br/>
+          Have a fantastic day!<br/><br/>
+          Dylan for TPAPIR<br/>
+          <span style="font-size: 12px;"><i>Customer Markets Agent</i></span>
+          <br/><br/>
+          ---<br/>
+          TAPIR Inc.<br/>
+          Phone: +1 234 567 89 10<br/>
+          Fax: +1 234 567 89 10<br/>
+          Email: dylan@tpapir.rc3<br/>
+          
+          <div style="font-size: 12px; margin-top: 20px;">
+            <i>"A wonderful softness has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. And I wish the same for you!"</i><br>
+            - Jim Stone, Founder of TPAPIR
+          </div>
+        </div>
+        <div style="border-top: 1px solid #efefef; margin-top:20px; font-size:10px; color:#aaa; padding-top:10px">
+        This message is confidential and intended solely for the addressee. Any use by third parties is prohibited. If you have received the files in error, please contact the sender and delete the files from any computer and disk.
+        </div>
+      </div>
     `,
     folder: "inbox",
     incoming: true
@@ -38,19 +90,44 @@ export default [
     created: "",
     title: "🙏🏻 Thank you for your order",
     content: `
-    Dear customer,<br/><br/>
-    we would like to thank you for the following order:
-    <br/><br/>
-    Order number: 50876471
-    <br/><br/>
-    1 x Fortune Teller (4 Rolls)                                             37,00 EUR<br/>
-    1 x Shipping                                                              3,80 EUR<br/>
-    ----------------------------------------------------------------------------------<br/>
-    Total amount including all fees and VAT:                                 40,80 EUR<br/>
-    <br/><br/>
-    Payment: online by credit card
-    <br/><br/>
-    Your TPAPIR team
+    <div style="width: 500px; margin: 0 auto;">
+        <header style="border-bottom: 1px solid #efefef; padding: 20px 0 20px 0;">
+          <img src="${logo}" height="30"/><span style="margin-left: 10px; font-weight: 500; font-size: 20px; line-height: 20px; vertical-align:middle;">TPAPIR</span>
+          <div style="margin-top: 20px;"><i>It is not just paper, it is the expression of your innermost need.</i></div>
+        </header>
+        <div style="margin-top: 20px;">
+          Dear customer,<br/><br/>
+          we would like to thank you for the following order:
+          <br/><br/>
+          Order number: 50876471
+          <br/><br/>
+          1 x Fortune Teller (4 Rolls)                                             37,00 EUR<br/>
+          1 x Shipping                                                              3,80 EUR<br/>
+          ----------------------------------------------------------------------------------<br/>
+          Total amount including all fees and VAT:                                 40,80 EUR<br/>
+          <br/><br/>
+          Payment: online by credit card
+          <br/><br/>
+          And do not forget: With TPAPIR you back the right roll!<br/>
+          Have a fantastic day!<br/><br/>
+          Dylan for TPAPIR<br/>
+          <span style="font-size: 12px;"><i>Customer Markets Agent</i></span>
+          <br/><br/>
+          ---<br/>
+          TAPIR Inc.<br/>
+          Phone: +1 234 567 89 10<br/>
+          Fax: +1 234 567 89 10<br/>
+          Email: dylan@tpapir.rc3<br/>
+          
+          <div style="font-size: 12px; margin-top: 20px;">
+            <i>"A wonderful softness has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. And I wish the same for you!"</i><br>
+            - Jim Stone, Founder of TPAPIR
+          </div>
+        </div>
+        <div style="border-top: 1px solid #efefef; margin-top:20px; font-size:10px; color:#aaa; padding-top:10px">
+        This message is confidential and intended solely for the addressee. Any use by third parties is prohibited. If you have received the files in error, please contact the sender and delete the files from any computer and disk.
+        </div>
+      </div>
     `,
     folder: "inbox",
     incoming: true
@@ -61,12 +138,38 @@ export default [
     created: "",
     title: "💁‍♀️ Your order was processed",
     content: `
-    Dear customer,<br/><br/>
-    your order was processed.
-    <br/><br/>
-    You can expect to hear back from us as soon as your order is ready for packaging.
-    <br/><br/>
-    Your TPAPIR team`,
+    <div style="width: 500px; margin: 0 auto;">
+        <header style="border-bottom: 1px solid #efefef; padding: 20px 0 20px 0;">
+          <img src="${logo}" height="30"/><span style="margin-left: 10px; font-weight: 500; font-size: 20px; line-height: 20px; vertical-align:middle;">TPAPIR</span>
+          <div style="margin-top: 20px;"><i>It is not just paper, it is the expression of your innermost need.</i></div>
+        </header>
+        <div style="margin-top: 20px;">
+          Dear customer,<br/><br/>
+          your order was processed.
+          <br/><br/>
+          You can expect to hear back from us as soon as your order is ready for packaging.
+          <br/><br/>
+          And do not forget: With TPAPIR you back the right roll!<br/>
+          Have a fantastic day!<br/><br/>
+          Dylan for TPAPIR<br/>
+          <span style="font-size: 12px;"><i>Customer Markets Agent</i></span>
+          <br/><br/>
+          ---<br/>
+          TAPIR Inc.<br/>
+          Phone: +1 234 567 89 10<br/>
+          Fax: +1 234 567 89 10<br/>
+          Email: dylan@tpapir.rc3<br/>
+          
+          <div style="font-size: 12px; margin-top: 20px;">
+            <i>"A wonderful softness has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. And I wish the same for you!"</i><br>
+            - Jim Stone, Founder of TPAPIR
+          </div>
+        </div>
+        <div style="border-top: 1px solid #efefef; margin-top:20px; font-size:10px; color:#aaa; padding-top:10px">
+        This message is confidential and intended solely for the addressee. Any use by third parties is prohibited. If you have received the files in error, please contact the sender and delete the files from any computer and disk.
+        </div>
+      </div>
+    `,
     folder: "inbox",
     incoming: true
   },
@@ -76,12 +179,38 @@ export default [
     created: "",
     title: "📦 Your order was scheduled for packaging",
     content: `
-    Dear customer,<br/><br/>
-    your order is was scheduled and will be packed soon.
-    <br/><br/>
-    If you have any questions please reach out.
-    <br/><br/>
-    Your TPAPIR team`,
+    <div style="width: 500px; margin: 0 auto;">
+        <header style="border-bottom: 1px solid #efefef; padding: 20px 0 20px 0;">
+          <img src="${logo}" height="30"/><span style="margin-left: 10px; font-weight: 500; font-size: 20px; line-height: 20px; vertical-align:middle;">TPAPIR</span>
+          <div style="margin-top: 20px;"><i>It is not just paper, it is the expression of your innermost need.</i></div>
+        </header>
+        <div style="margin-top: 20px;">
+          Dear customer,<br/><br/>
+          your order is was scheduled and will be packed soon.
+          <br/><br/>
+          If you have any questions please reach out.
+          <br/><br/>
+          And do not forget: With TPAPIR you back the right roll!<br/>
+          Have a fantastic day!<br/><br/>
+          Dylan for TPAPIR<br/>
+          <span style="font-size: 12px;"><i>Customer Markets Agent</i></span>
+          <br/><br/>
+          ---<br/>
+          TAPIR Inc.<br/>
+          Phone: +1 234 567 89 10<br/>
+          Fax: +1 234 567 89 10<br/>
+          Email: dylan@tpapir.rc3<br/>
+          
+          <div style="font-size: 12px; margin-top: 20px;">
+            <i>"A wonderful softness has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. And I wish the same for you!"</i><br>
+            - Jim Stone, Founder of TPAPIR
+          </div>
+        </div>
+        <div style="border-top: 1px solid #efefef; margin-top:20px; font-size:10px; color:#aaa; padding-top:10px">
+        This message is confidential and intended solely for the addressee. Any use by third parties is prohibited. If you have received the files in error, please contact the sender and delete the files from any computer and disk.
+        </div>
+    </div>
+    `,
     folder: "inbox",
     incoming: true
   },
@@ -91,10 +220,36 @@ export default [
     created: "",
     title: "🧚 Your order was handled and is prepared for shipping",
     content: `
-    Dear customer,<br/><br/>
-    we are now preparing your order for shipping.
-    <br/><br/>
-    Your TPAPIR team`,
+    <div style="width: 500px; margin: 0 auto;">
+        <header style="border-bottom: 1px solid #efefef; padding: 20px 0 20px 0;">
+          <img src="${logo}" height="30"/><span style="margin-left: 10px; font-weight: 500; font-size: 20px; line-height: 20px; vertical-align:middle;">TPAPIR</span>
+          <div style="margin-top: 20px;"><i>It is not just paper, it is the expression of your innermost need.</i></div>
+        </header>
+        <div style="margin-top: 20px;">
+          Dear customer,<br/><br/>
+          we are now preparing your order for shipping.
+          <br/><br/>
+          And do not forget: With TPAPIR you back the right roll!<br/>
+          Have a fantastic day!<br/><br/>
+          Dylan for TPAPIR<br/>
+          <span style="font-size: 12px;"><i>Customer Markets Agent</i></span>
+          <br/><br/>
+          ---<br/>
+          TAPIR Inc.<br/>
+          Phone: +1 234 567 89 10<br/>
+          Fax: +1 234 567 89 10<br/>
+          Email: dylan@tpapir.rc3<br/>
+          
+          <div style="font-size: 12px; margin-top: 20px;">
+            <i>"A wonderful softness has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. And I wish the same for you!"</i><br>
+            - Jim Stone, Founder of TPAPIR
+          </div>
+        </div>
+        <div style="border-top: 1px solid #efefef; margin-top:20px; font-size:10px; color:#aaa; padding-top:10px">
+        This message is confidential and intended solely for the addressee. Any use by third parties is prohibited. If you have received the files in error, please contact the sender and delete the files from any computer and disk.
+        </div>
+      </div>
+    `,
     folder: "inbox",
     incoming: true
   },
@@ -104,16 +259,42 @@ export default [
     created: "",
     title: "🤷 We are very sorry",
     content: `
-    Dear customer,<br/><br/>
-    we are reaching out to inform you of an incident.<br/>
-    We have detected an issue with our internal system.
-    <br/><br/>
-    Unfortunately, your order has been destroyed.
-    <br/><br/>
-    I’d like to apologize for the impact this is having on you.<br/><br/>
-    Please be assured that we will do everything we can to prevent this from happening again.
-    <br/><br/>
-    Your TPAPIR team`,
+    <div style="width: 500px; margin: 0 auto;">
+        <header style="border-bottom: 1px solid #efefef; padding: 20px 0 20px 0;">
+          <img src="${logo}" height="30"/><span style="margin-left: 10px; font-weight: 500; font-size: 20px; line-height: 20px; vertical-align:middle;">TPAPIR</span>
+          <div style="margin-top: 20px;"><i>It is not just paper, it is the expression of your innermost need.</i></div>
+        </header>
+        <div style="margin-top: 20px;">
+          Dear customer,<br/><br/>
+          we are reaching out to inform you of an incident.<br/>
+          We have detected an issue with our internal system.
+          <br/><br/>
+          Unfortunately, your order has been destroyed.
+          <br/><br/>
+          I’d like to apologize for the impact this is having on you.<br/><br/>
+          Please be assured that we will do everything we can to prevent this from happening again.
+          <br/><br/>
+          And do not forget: With TPAPIR you back the right roll!<br/>
+          Have a fantastic day!<br/><br/>
+          Dylan for TPAPIR<br/>
+          <span style="font-size: 12px;"><i>Customer Markets Agent</i></span>
+          <br/><br/>
+          ---<br/>
+          TAPIR Inc.<br/>
+          Phone: +1 234 567 89 10<br/>
+          Fax: +1 234 567 89 10<br/>
+          Email: dylan@tpapir.rc3<br/>
+          
+          <div style="font-size: 12px; margin-top: 20px;">
+            <i>"A wonderful softness has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. And I wish the same for you!"</i><br>
+            - Jim Stone, Founder of TPAPIR
+          </div>
+        </div>
+        <div style="border-top: 1px solid #efefef; margin-top:20px; font-size:10px; color:#aaa; padding-top:10px">
+        This message is confidential and intended solely for the addressee. Any use by third parties is prohibited. If you have received the files in error, please contact the sender and delete the files from any computer and disk.
+        </div>
+      </div>
+    `,
     folder: "inbox",
     incoming: true,
     attachment: "attachment-retrograde.jpg"
